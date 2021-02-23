1529b6c7660ebae613a46c911dac93a5c7726a7e
kimjinsoo <roorooro9933@gmail.com>
2021-02-23 17:50:20 +0900

    understanding CORS Error and how to solve this problem with setting http headers

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const feedRoutes = require("./routes/feed");
+const { restart } = require("nodemon");
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+});
 
 app.use("/feed", feedRoutes);
 

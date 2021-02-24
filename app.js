7ab44209d77200487d4817a3338afceda0ab6a66
kimjinsoo <roorooro9933@gmail.com>
2021-02-24 16:29:17 +0900

    fix issue about path in window

--- app.js
+++ app.js
@@ -3,12 +3,38 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongooseConnect = require("./utils/mogooseConnect.s");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4());
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 app.use(bodyParser.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {

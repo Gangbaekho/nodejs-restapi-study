baaf5c7ee69016ffd68e15124fa315ce7177b716
kimjinsoo <roorooro9933@gmail.com>
2021-02-24 16:06:03 +0900

    post controller logic and error throw and error handling middlware

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongooseConnect = require("./utils/mogooseConnect.s");
@@ -7,6 +9,7 @@ const feedRoutes = require("./routes/feed");
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -19,5 +22,12 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 mongooseConnect(app);

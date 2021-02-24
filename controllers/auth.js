f0ce165b0db307198d94f8037b040c5dddd8d0a1
kimjinsoo <roorooro9933@gmail.com>
2021-02-24 18:16:21 +0900

    how to send jwt token to client

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const { validationResult } = require("express-validator/check");
 const User = require("../models/user");
 
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SCRET_CODE = require("../utils/jwtScretKey.s");
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -29,6 +31,46 @@ exports.signup = (req, res, next) => {
     })
     .then((result) => {
       res.status(201).json({ message: "User created!", userId: result._id });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("A user with this email could not be found");
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Wrong password!");
+        error.statusCode = 401;
+        throw error;
+      }
+
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
+        JWT_SCRET_CODE,
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
     })
     .catch((error) => {
       if (!error.statusCode) {

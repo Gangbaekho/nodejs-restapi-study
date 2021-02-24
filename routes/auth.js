f735c68f93b4746207d719eac70d86d633c54ba8
kimjinsoo <roorooro9933@gmail.com>
2021-02-24 17:55:32 +0900

    add signup logic

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,31 @@
 const express = require("express");
 
+const User = require("../models/user");
+
+const { body } = require("express-validator/check");
+
+const authController = require("../controllers/auth");
+
 const router = express.Router();
 
-router.put("/signup");
+router.put(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a vaild email.")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject("E-mail address already exists!");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password").trim().isLength({ min: 5 }),
+    body("name").trim().not().isEmpty(),
+  ],
+  authController.signup
+);
 
 module.exports = router;
